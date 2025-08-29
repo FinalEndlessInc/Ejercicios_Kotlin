3db9069e0fd063aeb94017a492c76416e66e3e80
unknown <hoiimendless@gmail.com>
2025-08-28 21:38:23 -0600

    [EDITAR] Codigo modificado, el bebe ya puede entrar a batallas campales

--- Bebe_ejercicios.kt
+++ Bebe_ejercicios.kt
@@ -1,18 +1,18 @@
 import kotlin.random.Random
 
 enum class EstadosBebes{
-    despierto, //
+    despierto, 
     llorando, 
-    dormido, //
-    comiendo, //
+    dormido, 
+    comiendo, 
     cagado_y_miado, 
-    viendo_al_rasho_Macuin, //
+    viendo_al_rasho_Macuin, 
     esperando_SilkSong,
     bailando_la_macarena,
     dibujando,
-    gano, //
-    exploto, //
-    tieso //
+    gano,
+    exploto, 
+    tieso 
 }
 
 
@@ -25,14 +25,26 @@ class Bebe(
     var batalla: Int,
     var estado: EstadosBebes = EstadosBebes.despierto
 ){
-    fun alimentar(){
-        if(sigue_vivo() && esta_despierto()){
-            estado = EstadosBebes.comiendo
+
+    //Checador que siga vivo 💀☠️
+    fun sigue_vivo(): Boolean{
+        if(estado == EstadosBebes.tieso || estado == EstadosBebes.exploto){
+            return false;
+        }else{
+            return true;
         }
     }
     
+    
+    fun alimentar(){
+        if(sigue_vivo()){
+            estado = EstadosBebes.comiendo
+        }
+        return
+    }
+    
     fun llorando(){
-        if(sigue_vivo() && esta_despierto()){
+        if(sigue_vivo()){
             estado = EstadosBebes.llorando
         }
     }
@@ -60,33 +72,34 @@ class Bebe(
     }
     
     fun checar_panial(){
-        if(sigue_vivo() && esta_despierto()){
+        if(sigue_vivo()){
             if (panial_lleno > 80){
                 estado = EstadosBebes.cagado_y_miado
                 panial_lleno = 0;
             }
+        }
     }
     
     fun waiiitinngg(){
-        if(sigue_vivo() && esta_despierto()){
+        if(sigue_vivo()){
             estado = EstadosBebes.esperando_SilkSong
         }
     }
     
     fun bailando(){
-        if(sigue_vivo() && esta_despierto()){
+        if(sigue_vivo()){
             estado = EstadosBebes.bailando_la_macarena
         }
     }
     
     fun dibujar(){
-        if(sigue_vivo() && esta_despierto()){
+        if(sigue_vivo()){
             estado = EstadosBebes.dibujando
         }
     }
     
     fun despertar_con_agua(){
-        if(sigue_vivo){
+        if(sigue_vivo()){
             if(estado == EstadosBebes.dormido){
                 estado = EstadosBebes.tieso
             }    
@@ -95,14 +108,14 @@ class Bebe(
     }
     
     fun ver_Cars(){
-        if(sigue_vivo() && esta_despierto()){
+        if(sigue_vivo()){
                 estado = EstadosBebes.viendo_al_rasho_Macuin
         }
     }
     
     fun batalla_campal(){
-        if(sigue_vivo() && esta_despierto()){
-            batalla = Random.nextInt(0,100) - suenio;
+        if(sigue_vivo()){
+            batalla = Random.nextInt(0,100);
         
             if(batalla > 49){
                 estado = EstadosBebes.gano
@@ -113,23 +126,6 @@ class Bebe(
         
     }
     
-    //Checador que siga vivo 💀☠️
-    fun sigue_vivo(): Boolean{
-        if(estado == EstadosBebes.tieso || estado == EstadosBebes.exploto){
-            return false;
-        }else{
-            return true;
-        }
-    }
-    
-    //Checar que no este dormido 🦐
-    fun esta_despierto():Boolean{
-        if(estado == EstadosBebes.despierto){
-            return true;
-        }else{
-            return false;
-        }
-    }
     
     fun actualizar_estado(){
         
@@ -175,14 +171,13 @@ class Bebe(
             }
             
             else ->{
-                
+                suenio +=10
+                hambre += 10
+                panial_lleno += 10
             }
         }
     }
     
-    fun picar_con_un_palo(): String{
-        return estado_actual()
-    }
     
     fun estado_actual():String{
     
@@ -190,37 +185,37 @@ class Bebe(
             EstadosBebes.despierto -> {
                 return "El bebe esta despierto"
             }
-            EstadosBebes.llorando ->{
+            EstadosBebes.llorando -> {
                 return "El bebe esta llorando (callenlo porfavor)"
             }
-            EstadosBebes.cagado_y_miado ->{
+            EstadosBebes.cagado_y_miado -> {
                 return "El bebe esta todo cagado encima"
             }
-            EstadosBebes.viendo_al_rasho_Macuin ->{
+            EstadosBebes.viendo_al_rasho_Macuin -> {
                 return "El bebe anda viendo al rasho macuin, joer que basado"
             }
-            EstadosBebes.esperando_SilkSong ->{
+            EstadosBebes.esperando_SilkSong -> {
                 return "Esperando Silksong, ya mero sale bebe"
             }
-            EstadosBebes.bailando_la_macarena ->{
+            EstadosBebes.bailando_la_macarena -> {
                 return "Esta bailando la macarena, que alguien lo detenga"
             }
-            EstadosBebes.dibujar ->{
+            EstadosBebes.dibujando -> {
                 return "El bebe anda dibujando geis, que pedoo"
             }
-            EstadosBebes.tieso ->{
+            EstadosBebes.tieso -> {
                 return "Estoy muerto Otto"
             }
-            EstadosBebes.dormido ->{
+            EstadosBebes.dormido -> {
                 return "Esta dormido"
             }
-            EstadosBebes.comiendo ->{
+            EstadosBebes.comiendo -> {
                 return "Estoy comiendo"
             }
-            EstadosBebes.gano ->{
+            EstadosBebes.gano -> {
                 return "El bebe le ha ganado a una bomba atomica"
             }
-            EstadosBebes.exploto ->{
+            EstadosBebes.exploto -> {
                 return "GOKUUU AHHHHH---- *explota* "
             }
             else -> {
@@ -231,16 +226,26 @@ class Bebe(
 }
 
 fun main(){
-    val JackieShieras = Bebe("Jackie Shieras", 50, 10, 70,0)
-    val AnalBrito = Bebe("Alan Brito", 40, 20, 60, 0)
+    //val JackieShieras = Bebe("Jackie Shieras", 50, 10, 70, 0)
+    val AlanBrito = Bebe("Alan Brito", 40, 30, 60, 0)
     
-    JackieShieras.actualizar_estado()
-    println("El estado del bebe es ${JackieShieras.estado_actual()}")
-    JackieShieras.dormir()
-    println("El estado del bebe es ${JackieShieras.estado_actual()}")
-    JackieShieras.batalla_campal()
-    println("El estado del bebe es ${JackieShieras.estado_actual()}")
-    JackieShieras.dormir()
-    println("El estado del bebe es ${JackieShieras.estado_actual()}")
+    AlanBrito.despertar()
+    println("El estado actual del bebe es: ${AlanBrito.estado_actual()}")
+    AlanBrito.actualizar_estado()
     
+    AlanBrito.batalla_campal()
+    println("El estado actual del bebe es: ${AlanBrito.estado_actual()}")
+  
+    AlanBrito.ver_Cars()
+    println("El estado actual del bebe es: ${AlanBrito.estado_actual()}")
+    
+    AlanBrito.waiiitinngg()
+    println("El estado actual del bebe es: ${AlanBrito.estado_actual()}")
+    
+    AlanBrito.bailando()
+    println("El estado actual del bebe es: ${AlanBrito.estado_actual()}")
+
+    AlanBrito.dibujar()
+    println("El estado actual del bebe es: ${AlanBrito.estado_actual()}")
 }
+    
